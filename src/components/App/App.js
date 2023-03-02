eaed37a6895229b80fec25d28b45e3711f3018ef
John Mayou <johnm.ceo@gmail.com>
2023-03-02 13:37:42 -0600

    created navbar with title and two buttons that go to search and gallery pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+
+import Navbar from "../Navbar/Navbar";
 
 function App(props) {
-  return (
-    <div>
-      <h1>Giphy Search!</h1>
-    </div>
-  );
+	return (
+		<div>
+			<Router>
+				<Navbar />
+				<Route exact path="/">
+					<h1>Replace Search</h1>
+				</Route>
+				<Route exact path="/gallery">
+					<h1>Replace Gallery</h1>
+				</Route>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
