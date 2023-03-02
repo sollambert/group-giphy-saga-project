7024a89e19b1388eeeb8ad4b7f5821ea0adda54f
Anniessa Antar <anniessa.antar@gmail.com>
2023-03-02 14:15:50 -0600

    made some more changes to the gifList

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import GifList from '../GifList/GifList';
 
 function App(props) {
   return (
     <div>
       <h1>Giphy Search!</h1>
+      <GifList />
     </div>
   );
 }
--- src/components/GifList/GifList.jsx
+++ src/components/GifList/GifList.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import GifItem from '../GifItem/GifItem';
 
 function GifList() {
     const dispatch = useDispatch();
