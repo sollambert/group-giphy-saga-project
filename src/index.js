9a7b8461bee4c5d0276f48b02918e3863b8bda18
Solomon Lambert <sol.lambert@gmail.com>
2023-03-02 11:52:19 -0600

    initialize sagas and redux

--- src/index.js
+++ src/index.js
@@ -2,9 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 
+import axios from 'axios';
+
+//redux
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+//sagas
+import createSagaMiddleware from '@redux-saga/core';
+import { takeEvery, put } from 'redux-saga/effects';
+
+function* watcherSaga() {
+}
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+    combineReducers({
+
+    }),
+    applyMiddleware(sagaMiddleware, logger);
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
