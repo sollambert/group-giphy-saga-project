171561ceebf09c8e4c2482bf1c17ea6151ab7b1a
Solomon Lambert <sol.lambert@gmail.com>
2023-03-02 13:15:13 -0600

    create sagas for favorites and category views

--- src/index.js
+++ src/index.js
@@ -2,9 +2,70 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 
+import axios from 'axios';
+
+//redux
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+//sagas
+import createSagaMiddleware from '@redux-saga/core';
+import { takeEvery, put } from 'redux-saga/effects';
+
+//gallery reducer
+function galleryGifs(state = [], action) {
+    switch(action.type) {
+        case "SET_GALLERY":
+            return action.payload;
+        case "CLEAR_GALLERY":
+            return [];
+        default:
+            return state;
+    }
+}
+
+//get favorites saga
+function* getFavorites() {
+    try {
+        let response = yield axios.get('/favorite');
+        put({type: "SET_GALLERY", payload: response.data})
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+//get category saga
+function* getCategory(action) {
+    try {
+        let response = yield axios.get('/category', { category: action.payload });
+        put({type: "SET_GALLERY", payload: response.data})
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function* watcherSaga() {
+    yield takeEvery("GET_FAVORITES", getFavorites);
+    yield takeEvery("GET_CATEGORY", getCategory);
+}
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+    combineReducers({
+        galleryGifs
+    }),
+    applyMiddleware(sagaMiddleware, logger)
+)
+
+sagaMiddleware.run(watcherSaga);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
